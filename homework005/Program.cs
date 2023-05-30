86a0e4947c6224159b3e52f77dadaeecdce7c263
Sergei <sergei.guminyuk@gmail.com>
2023-05-30 19:31:46 +0300

    Add 005,006,007

---
+++ homework005/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine()!);
+int num1 = num / 100;
+int num2 =(num / 10) % 10;
+int num3 = num%10;
+
+
+while (num < 100 || num > 1000)
+
+    if (num < 100)
+    {
+        Console.WriteLine("Ошибка. Вы ввели число меньше трех цифр. ");
+        return;  
+    }
+else
+    {
+        Console.WriteLine("Ошибка. Вы ввели число больше трех цифр. ");
+        return;
+    }
+
+
+Console.WriteLine("Второе число :" + num2);
---
+++ homework007/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+
+int Prompt (string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+void ExamDay (int number)
+{
+    while (number <= 0 || number > 7)
+    {
+        Console.WriteLine("Введено неподходящее число, введите повторно: ");
+        number = int.Parse(Console.ReadLine ()!);
+    }
+    
+    if (number >= 1 && number <= 5)
+    {
+        Console.WriteLine("Это рабочий день. Нужен выходной. ");
+    }
+    
+    else if (number == 6 || number == 7)
+    {
+        Console.WriteLine("Наконец то выходной");
+    }
+    
+}
+
+int number1 = Prompt("Введите число приравненное к дню недели, пример: 1 понедельник, 7 воскресенье : ");
+ExamDay(number1);
